7f7edd305183f8b620060fe314b11cac4f912e05
Ezra Muskat <ezramuskat@gmail.com>
2023-04-10 19:46:33 -0400

    click count now reads from json db

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 
 function App() {
   //state hook for tracking clicks
   const [clickCount, setClickCount] = useState(0)
+
+  //function to get data from json
+  const getCountData = () => {
+    console.log('getting count data')
+    axios.get('http://localhost:3001/countData').then(response => {
+      console.log(response.data)
+      setClickCount(response.data.count)
+    })
+  }
+
+  useEffect(getCountData, [clickCount])
 
   //event handler for button clicks
   const handleClick = () => {
@@ -19,6 +31,7 @@ function App() {
           A nice spinning button. Click it and find out what happens.
         </p>
         <p>Click count: {clickCount}</p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
