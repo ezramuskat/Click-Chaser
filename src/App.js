69a70c73c09184b0451f610199576883f31d4c7f
Ezra Muskat <ezramuskat@gmail.com>
2023-04-10 18:26:42 -0400

    button now tracks clicks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react'
+
 function App() {
+  //state hook for tracking clicks
+  const [clickCount, setClickCount] = useState(0)
+
+  //event handler for button clicks
+  const handleClick = () => {
+    setClickCount(clickCount + 1)
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <button style={{background: 'transparent'}}><img src={logo} className="App-logo" alt="logo" /></button>
+        <button style={{background: 'transparent'}} onClick={handleClick}><img src={logo} className="App-logo" alt="logo" /></button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          A nice spinning button. Click it and find out what happens.
         </p>
+        <p>Click count: {clickCount}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
