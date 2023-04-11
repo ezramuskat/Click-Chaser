cea5fa5828d2daacb20721f24f38b93fa49134fb
Ezra Muskat <ezramuskat@gmail.com>
2023-04-10 22:12:48 -0400

    implemented geolocation detection

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import axios from 'axios';
 function App() {
   //state hook for tracking clicks
   const [clickCount, setClickCount] = useState(0)
+  const [geoData, setGeoData] = useState({})
 
   //function to get data from json
   const getCountData = () => {
@@ -17,7 +18,16 @@ function App() {
     })
   }
 
+  const getGeoData = () => {
+    console.log('getting geo data')
+    axios.get('http://localhost:3001/geoData').then(response => {
+      console.log(response.data)
+      setGeoData(response.data)
+    })
+  }
+
   useEffect(getCountData, [clickCount])
+  useEffect(getGeoData, [clickCount])
 
   //event handler for button clicks
   const handleClick = () => {
@@ -27,16 +37,40 @@ function App() {
     .then(response => {
       console.log(response.data)
     })
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      const country = await getCountry(position.coords.latitude, position.coords.longitude)
+      const posVal = geoData[country] ? geoData[country] + 1 : 1
+      console.log("posVal",posVal)
+      setGeoData({...geoData, [country]: posVal})
+      console.log("geoData", geoData)
+      axios.post('http://localhost:3001/geoData', {[country]: posVal})
+      .then(response => {
+        console.log("response", response.data)
+      })
+    })
+  }
+
+  async function getCountry(latitude, longitude) {
+    const apiCall = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`);
+    const countryData = await apiCall.json();
+    console.log(countryData)
+    return countryData.countryName;
   }
   return (
     <div className="App">
       <header className="App-header">
-        <button style={{background: 'transparent'}} onClick={handleClick}><img src={logo} className="App-logo" alt="logo" /></button>
-        <p>
+        <h1>
           A nice spinning button. Click it and find out what happens.
-        </p>
+        </h1>
+        <button style={{background: 'transparent'}} onClick={handleClick}><img src={logo} className="App-logo" alt="logo" /></button>
+        
         <p>Click count: {clickCount}</p>
-
+        <p>See where other people who clicked the button are from:</p>
+        <ul>
+          {Object.keys(geoData).map((key, index) => (
+            <li key={index}>{key}: {geoData[key]}</li>
+          ))}
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
